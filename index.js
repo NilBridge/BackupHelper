c62a1d10845f334e04b83384317715b2cc3b6e15
Sbaoor <2959435045@qq.com>
2022-03-30 09:49:51 +0800

    update

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const version = '1.1.2';
 function onStart(api) {
     api.regCMD("backup",'备份', (arg) => {
         if (NIL.SERVERS.has(arg[0]) == false) {
-            api.logger.warn(`没有名为${arg[0]}的服务器`);
-            return;
+            return `没有名为${arg[0]}的服务器`;
         }
         backup_item(arg[0]);
-        api.logger.info('备份已执行');
+         return '备份已执行';
     });
     api.logger.info(`BackupHelper存档备份助手-已装载  当前版本${version}`);
-    api.logger.info('作者：LItion   发布平台：MineBBS/Github');
+    api.logger.info('作者：Lition   发布平台：MineBBS/Github');
     api.logger.info('欲联系作者可前往MineBBS论坛或Github');
-    //NIL.bot.sendMainMessage(`[NilBridge备份助手]\nBackupHelper存档备份助手-已装载  当前版本${version}\n作者：LItion   发布平台：MineBBS/Github`)
 }
 
 
